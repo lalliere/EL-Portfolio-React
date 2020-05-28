2948ab4cf1752836711ddc44cae2488104ccc8ee
lalliere <emilylallier@yahoo.com>
2020-05-28 15:52:14 -0400

    upd imports and return

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import "materialize-css";
+import 'materialize-css/dist/css/materialize.min.css';
 import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Project from "./components/Project";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Project />
+      <Footer />
     </div>
   );
 }
