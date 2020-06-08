5647802dfc82d448cef85e69c86dd69803ba8151
lalliere <emilylallier@yahoo.com>
2020-06-07 20:51:08 -0400

    added all components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import React from 'react';
-import "materialize-css";
-import 'materialize-css/dist/css/materialize.min.css';
 import './App.css';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Project from "./components/Project";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
+import Footer from './components/Footer';
+import AboutMe from './components/AboutMe';
+import Carousel from "./components/Carousel";
+import Project from './components/Project';
+import projects from "./projects.json";
+import Routes from './components/Routes';
+import Grid from '@material-ui/core/Grid';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Project />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    projects
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Routes />
+        <Wrapper>
+          <AboutMe />
+          <hr></hr>
+          <Grid container spacing={2}>
+              {this.state.projects.map(project => (
+                <Grid item spacing={2} >
+                  <Project 
+                    img={project.img}
+                    alt={project.imgAlt}
+                    title={project.title}
+                    description={project.description}
+                    deployed={project.deployed}
+                    github={project.github}              
+                  />
+                </Grid>
+              ))}
+          </Grid>
+          <hr></hr>
+          <Carousel />
+        </Wrapper>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
