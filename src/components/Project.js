848aaeed3cc9f925ea30e46654eab5ec6db7f74c
lalliere <emilylallier@yahoo.com>
2020-06-10 16:28:53 -0400

    added cardActionArea

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
@@ -29,35 +30,40 @@ const useStyles = makeStyles({
         paddingTop: "0",
         marginTop: "0",
         fontSize: "14pt"
-    }
+    },
+    // media: {
+    //     height: 140,
+    // },
 });
 
 export default function Project(props) {
     const classes = useStyles();
 
     return (
-        <Card className={classes.root}>
-            <CardMedia
-                component="img"
-                alt={props.imgAlt}
-                height="140"
-                src={props.img}
-                title={props.title}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="h2"> {props.title}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">{props.description}
-                </Typography>
-            </CardContent>
-
+        <Card className={classes.root} elevation={3}>
+            <CardActionArea>
+                <CardMedia
+                    // className={classes.media}
+                    component="img"
+                    alt={props.imgAlt}
+                    height="194px"
+                    src={props.img}
+                    title={props.title}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2"> {props.title}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">{props.description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
             <CardActions>
                 <Button size="small" color="primary" target="_blank" href={props.deployed} rel="noopener">
                     View Site
-                            </Button>
+                </Button>
                 <Button size="small" color="primary" target="_blank" href={props.github} rel="noopener">
                     See the Code
-                            </Button>
+                </Button>
             </CardActions>
         </Card>
 
