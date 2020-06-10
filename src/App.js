3ff69fc347927b1bbd0ff4d29779ea5142e7f1b7
lalliere <emilylallier@yahoo.com>
2020-06-10 18:48:31 -0400

    took out state

--- src/App.js
+++ src/App.js
@@ -10,22 +10,22 @@ import projects from "./projects.json";
 import Routes from './components/Routes';
 import Grid from '@material-ui/core/Grid';
 
+
+
 class App extends React.Component {
-  state = {
-    projects
-  };
 
   render() {
     return (
+      
       <div className="App">
         <Navbar />
         <Routes />
         <Wrapper>
           <AboutMe />
           <hr></hr>
-          <Grid container spacing={2}>
-              {this.state.projects.map(project => (
-                <Grid item spacing={2} >
+          <Grid container spacing={10}>
+              {projects.map(project => (
+                <Grid item spacing={3} >
                   <Project 
                     img={project.img}
                     alt={project.imgAlt}
@@ -36,8 +36,8 @@ class App extends React.Component {
                   />
                 </Grid>
               ))}
+              <Grid item xs={12} />
           </Grid>
-          <hr></hr>
           <Carousel />
         </Wrapper>
         <Footer />
