039f30910bfc546380f32fe77e654521464eab24
lalliere <emilylallier@yahoo.com>
2020-06-10 18:47:20 -0400

    added programming lang route

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import AboutMe from "./AboutMe";
 import Project from "./Project";
+import Carousel from "./Carousel"
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>     
             <Route exact path="/about" component={AboutMe} />
             <Route exact path="/project" component={Project} />
-            {/* <Route exact path="/" component={Projects} /> */}
+            <Route exact path="/languages" component={Carousel} />
         </Switch>
     </BrowserRouter>
 )
