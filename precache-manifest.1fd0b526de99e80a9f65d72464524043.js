a2b329794ec54adaa769d5e436891a02897286aa
lalliere <emilylallier@yahoo.com>
2020-06-11 20:35:46 -0400

    Updates

---
+++ precache-manifest.1fd0b526de99e80a9f65d72464524043.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5567dcd9f1ce1b522fce72bc04f4bac",
+    "url": "/EL-Portfolio-React/index.html"
+  },
+  {
+    "revision": "0b7dd6ee20bc9e50509a",
+    "url": "/EL-Portfolio-React/static/css/main.ea04fb87.chunk.css"
+  },
+  {
+    "revision": "4850f67f92c9587ac7e4",
+    "url": "/EL-Portfolio-React/static/js/2.5b047677.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/EL-Portfolio-React/static/js/2.5b047677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b7dd6ee20bc9e50509a",
+    "url": "/EL-Portfolio-React/static/js/main.e6321ab5.chunk.js"
+  },
+  {
+    "revision": "2d7089e2b7bf4088b691",
+    "url": "/EL-Portfolio-React/static/js/runtime-main.19a3b2f7.js"
+  },
+  {
+    "revision": "92950c754d1f6c6e57d8385090fdddd2",
+    "url": "/EL-Portfolio-React/static/media/abovecurve.92950c75.png"
+  },
+  {
+    "revision": "a1e50d5abf142fada76aa13958ce88df",
+    "url": "/EL-Portfolio-React/static/media/bbLogo.a1e50d5a.png"
+  },
+  {
+    "revision": "06b313b267c631d2ed297d81fdc75348",
+    "url": "/EL-Portfolio-React/static/media/burgerapp.06b313b2.png"
+  },
+  {
+    "revision": "c82bb3d9e5a5e67958f1eb17cc9e3dcf",
+    "url": "/EL-Portfolio-React/static/media/empDirReact.c82bb3d9.png"
+  },
+  {
+    "revision": "be8cec6f7e2d91e9b1d3e8d9ad4a3b66",
+    "url": "/EL-Portfolio-React/static/media/headshot3.be8cec6f.jpg"
+  },
+  {
+    "revision": "935827086f2ddbda323c056491c415d1",
+    "url": "/EL-Portfolio-React/static/media/hikeshops.93582708.png"
+  },
+  {
+    "revision": "3fa3c67459ec12940c6a8a1c07592f2e",
+    "url": "/EL-Portfolio-React/static/media/notetaker.3fa3c674.png"
+  }
+]);
