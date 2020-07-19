4c27b6b9d934ea539d48997f845e1f45c7d5b65a
lalliere <emilylallier@yahoo.com>
2020-07-19 11:58:17 -0400

    styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Grid from "@material-ui/core/Grid";
 import { BrowserRouter } from "react-router-dom";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import Logo from "../images/logoPinkCircleThick.png";
 
 // de6161 916C80
 const useStyles = makeStyles((theme) => ({
@@ -15,14 +16,18 @@ const useStyles = makeStyles((theme) => ({
         border: 0,
         borderRadius: 3,
         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-        color: 'white',
-        height: 48,
+        color: '#E5DCE1',
+        height: 52,
+    },
+    logo: {
+        height: "4vw",
+        paddingBottom: "5px"
     },
     title: {
         fontSize: '24pt',
     },
     links: {
-        padding: theme.spacing(1),
+        padding: theme.spacing(3),
         textDecoration: "none",
         color: "#ffffff",
         '&:hover': {
@@ -44,14 +49,17 @@ export default function NavBar() {
                 <Toolbar>
                     <Grid justify="space-between" alignItems="center" container spacing={4}>
                         <Grid item>
-                            <Typography variant="h6" className={classes.title}>
-                            Emily Lallier
-                            </Typography>
+                            <img
+                            src={Logo}
+                            alt="Emily Lallier Logo"
+                            className={classes.logo}
+                            >
+                            </img>
                         </Grid>
                         <Grid item>
                             <BrowserRouter>
                                 <Typography  variant="h6">
-                                    <AnchorLink className={ classes.links } href="#aboutme">Contact</AnchorLink>
+                                    <AnchorLink className={ classes.links } href="#contact">Contact</AnchorLink>
                                     <AnchorLink className={ classes.links } href="#aboutme">About Me</AnchorLink>
                                     <AnchorLink className={ classes.links } href="#portfolio">Portfolio</AnchorLink>
                                     <AnchorLink className={ classes.links } href="#languages">Programming Languages</AnchorLink>
